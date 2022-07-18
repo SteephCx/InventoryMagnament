41ac742c14043b9c4a3000644962c66f3f916491
Stephanie R Mapula <63594393+SteephCx@users.noreply.github.com>
2022-07-18 16:29:37 -0600

    Init

---
+++ app/Http/Controllers/Demo/DemoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Demo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    public function Index(){
+         return view('about');
+    }
+
+    public function ContactMethod(){
+        return view('contact');
+   }
+}
