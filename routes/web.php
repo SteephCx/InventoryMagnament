321678bd9d4f443eb7231df3f5b16462d75b502d
Stephanie R Mapula <63594393+SteephCx@users.noreply.github.com>
2022-07-19 16:37:06 -0600

    admin master blade

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::controller(DemoController::class)->group(function(){
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
