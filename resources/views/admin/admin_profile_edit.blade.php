b4e336fae279960f8de7d4bab213a58e57ebcef2
Stephanie R Mapula <63594393+SteephCx@users.noreply.github.com>
2022-07-28 00:00:38 -0600

    image

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.admin_master')
 @section('admin')
 <script src="https://cdn.tailwindcss.com"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <div class="page-content">
 <div class="container-fluid">
     <h4 class="uppercase content-center text-center mb-5"> editar perfil</h4>
@@ -9,20 +10,24 @@
         <div class="flex flex-wrap -mx-3 mb-6">
 
           <div class="w-full  px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs
+             font-bold mb-2" for="grid-first-name">
               Nombre
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
-             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
+              focus:bg-white"
              id="name" type="text" name="name" value="{{$editData->name}}">
           </div>
 
           <div class="w-full  px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold
+             mb-2" for="grid-first-name">
               Correo electronico
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
-             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
+              focus:bg-white"
              id="email" type="email" name="email"  value="{{$editData->email}}">
           </div>
           <div class="w-full  px-3 mb-6 md:mb-0">
@@ -30,20 +35,24 @@
               Usuario
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
-             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
+              focus:bg-white"
              id="username" type="text" name="username" value="{{$editData->username}}">
           </div>
           <div class="w-full md px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs
+            font-bold mb-2" for="grid-first-name">
               Imagen de perfil
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border
-             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none
+              focus:bg-white"
              id="profile_image" type="file" name="profile_image">
           </div>
 <center>
           <div class="rounded w-full xl md:mb-0">
-            <img class="ml-8 rounded-circle avatar-xl " src="{{asset('backend/assets/images/small/img-5.jpg')}}">
+            <img class="ml-8 rounded-circle avatar-xl " id="showImage"
+            src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
 
           </div>
 </center>
@@ -60,4 +69,17 @@
 
 </div>
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 @endsection
